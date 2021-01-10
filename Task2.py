0447b06dab71b057ff0d749d3b7dab2094f9e89f
dekelme <61948586+dekelme@users.noreply.github.com>
2021-01-10 19:54:30 +0200

    update

--- Task2.py
+++ Task2.py
@@ -1,13 +1,35 @@
 from Main import *
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
 
 def Task2():
     # 1.1
+    # option 1:
+    #  df1 = pd.read_csv('mobile_price_1.csv')
+    # corr = df1.corr()
+    # sns.heatmap(corr, cmap='YlGnBu', vmin=-1, vmax=1)
+    # plt.show()
+    # option 2:
     df1 = pd.read_csv('mobile_price_1.csv')
-    corr = df1.corr()
-    sns.heatmap(corr, cmap='YlGnBu', vmin=-1, vmax=1)
+    X = df1.iloc[:, 0:20]
+    y = df1.iloc[:, -1]
+    corrmat = df1.corr()
+    top_corr_features = corrmat.index
+    plt.figure(figsize=(20, 20))
+    map = sns.heatmap(df1[top_corr_features].corr(), annot=True, cmap="YlGnBu")
     plt.show()
-
     # 1.2
+
+
+    # X = df1.iloc[:, 0:20]
+    # y = df1.iloc[:, -1]
+    # bestfeatures = SelectKBest(score_func=chi2, k=10)
+    # fit = bestfeatures.fit(X, y)
+    # dfscores = pd.DataFrame(fit.scores_)
+    # dfcolumns = pd.DataFrame(X.columns)
+    # featureScores = pd.concat([dfcolumns, dfscores], axis=1)
+    # featureScores.columns = ['Specs', 'Score']
+    # print(featureScores.nlargest(10, 'Score'))
 # ______________________________
 # data = np.genfromtxt("mobile_price_1.csv", delimiter=",",skip_header=True, usecols=range(1, 11))
 # plt.imshow(data, cmap='hot', interpolation='nearest')
